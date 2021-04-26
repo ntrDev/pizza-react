85814840f4c1cab324b3b1e9ce1a5e796d3fe129
nth <dima.kolyshnitsyn@gmail.com>
2021-04-26 23:12:42 +0300

    create components ,create react-router

---
+++ src/components/SortPopUp.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+
+
+const SortPopUp = ({items}) => {
+    const [visiblePopUp,setVisiblePopUp] = React.useState(false);  
+    const [activeItems,setActiveItems] = React.useState(0);  
+    const activeLabel = items[activeItems];
+    const  onSelectedItems = (index) => {
+        setActiveItems(index);
+        setVisiblePopUp(false);
+    }
+    const sortRef = React.useRef();
+    const handleOutsideClick = (e) => {
+        if(!e.path.includes(sortRef.current)){
+            setVisiblePopUp(false);
+        }
+    }
+    const togglePopUp = () => {
+        setVisiblePopUp(!visiblePopUp);
+    } 
+
+	React.useEffect(() => {
+		document.body.addEventListener('click',handleOutsideClick)
+		
+	}, [])
+
+	return (
+		<div ref={sortRef} className="sort">
+    	<div className="sort__label">
+            <svg
+                className={visiblePopUp ? 'rotated' : ''}
+                width="10"
+                height="6"
+                viewBox="0 0 10 6"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg">
+
+				<path
+					d="M10 5C10 5.16927 9.93815 5.31576 9.81445 5.43945C9.69075 5.56315 9.54427 5.625 9.375 5.625H0.625C0.455729 5.625 0.309245 5.56315 0.185547 5.43945C0.061849 5.31576 0 5.16927 0 5C0 4.83073 0.061849 4.68424 0.185547 4.56055L4.56055 0.185547C4.68424 0.061849 4.83073 0 5 0C5.16927 0 5.31576 0.061849 5.43945 0.185547L9.81445 4.56055C9.93815 4.68424 10 4.83073 10 5Z"
+					fill="#2C2C2C"/>
+            </svg>
+            <b>Сортировка по:</b>
+            <span onClick = {togglePopUp}>{activeLabel}</span>
+        </div>
+        {   visiblePopUp && <div className="sort__popup">
+                <ul>
+                {items && items.map((name,index) => (
+                    <li 
+                        className={activeItems === index ? 'active' : ''  }
+                        onClick={() => onSelectedItems(index)}
+                        key={`${name}_${index}`}>{name}
+                    </li>))
+                }
+                </ul>
+            </div>
+        }
+    </div>
+	)
+}
+
+export default SortPopUp;
+
+	
+	
+	
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,4 @@
 export { default as Header} from './Header';
+
 export { default as Categories} from './Categories';
+export { default as SortPopUp} from './SortPopUp';
